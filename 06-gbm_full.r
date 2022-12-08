4541651483128108c176ec5796d9d2f12f75fead
Ludvig <ludvig@clin.au.dk>
2022-12-08 18:31:39 +0100

    Makes gbm model runnable

--- 06-gbm_full.r
+++ 06-gbm_full.r
@@ -1,100 +1,126 @@
-# Keeping this code for reference
 
-#library(tidyverse)
-# library(caret)
-# library(pROC)
+library(tidyverse)
+library(caret)
+library(pROC)
+library(optparse)
+library(stringr)
 
-# The feature creation was moved to `04-5mb_bins.r` for simplicity
-# df.fr3 <- readRDS("../inst/extdata/bins_5mbcompartments.rds")
-# # summary.df <- readRDS("sample_reference.csv")
-#
-# features.cov <- df.fr3  %>% ungroup() %>%
-#     select(nfrags.corrected2, sample, bin) %>%
-#     spread(sample, nfrags.corrected2) %>%
-#     select(-bin) %>%
-#     na.omit() %>%
-#     scale() %>%
-#     t() %>%
-#     as.data.frame()
-#
-# features.short <- df.fr3  %>% ungroup() %>%
-#     select(short.corrected2, sample, bin) %>%
-#     spread(sample, short.corrected2) %>%
-#     select(-bin) %>%
-#     na.omit() %>%
-#     scale() %>%
-#     t() %>%
-#     as.data.frame()
-#
-# features.sl <- cbind(features.cov, features.short)
-# colnames(features.sl) <- c(paste0("total", 1:498), paste0("short", 1:498))
-# features.sl$type <- ifelse(summary.df$type == "Healthy", "Healthy", "Cancer")
+# Command line arguments
+option_list = list(
+  make_option(
+    c("-i", "--in_features_file"),
+    action = "store",
+    default = NA,
+    type = 'character',
+    help = "A file with the short+total features. Must have the extension `.csv`."
+  ),
+  make_option(
+    c("-m", "--in_meta_file"),
+    action = "store",
+    default = NA,
+    type = 'character',
+    help = "A file where the first 2 columns contain the sample IDs and labels. Must have the extension `.csv`."
+  ),
+  make_option(
+    c("-o", "--out_model_file"),
+    action = "store",
+    default = NA,
+    type = 'character',
+    help = "The filepath to save the model at. Must have the extension `.rds`."
+  ),
+  make_option(
+    c("-p", "--out_preds_file"),
+    action = "store",
+    default = NA,
+    type = 'character',
+    help = "The filepath to save the predictions at. Must have the extension `.csv`."
+  )
+)
+opt = parse_args(OptionParser(option_list = option_list))
+if (is.na(opt$in_features_file)){
+  stop("--in_features_file was not specified.")
+}
+if (is.na(opt$in_meta_file)){
+  stop("--in_meta_file was not specified.")
+}
+if (is.na(opt$out_model_file)){
+  stop("--out_model_file was not specified.")
+}
+if (is.na(opt$out_preds_file)){
+  stop("--out_preds_file was not specified.")
+}
 
-# TODO Save short/long features
+if (str_sub(opt$in_features_file, start= -4) != ".csv"){
+  stop("--in_features_file must have the extension '.csv'.")
+}
+if (str_sub(opt$in_meta_file, start= -4) != ".csv"){
+  stop("--in_meta_file must have the extension '.csv'.")
+}
+if (str_sub(opt$out_model_file, start= -4) != ".rds"){
+  stop("--out_model_file must have the extension '.rds'.")
+}
+if (str_sub(opt$out_preds_file, start= -4) != ".csv"){
+  stop("--out_preds_file must have the extension '.csv'.")
+}
 
+# Load in features and add labels
+features.sl <- read_csv(opt$in_features_file)
+meta_data <- read_csv(opt$in_meta_file)
+features.sl["type"] <- meta_data[[2]]
 
+print("Loaded features and labels: ")
+print(head(features.sl, 10))
 
+ctrl <- trainControl(
+  method = "repeatedcv",
+  number = 10,
+  repeats = 10,
+  verboseIter = FALSE,
+  savePredictions = TRUE,
+  classProbs = TRUE,
+  # preProcOptions=list(thres = 0.90),
+  summaryFunction = twoClassSummary
+)
 
-#
-# features <- cbind(features.sl,
-#              as.matrix(summary.df %>% ungroup() %>%
-#                        select(contains("Z Score"))))
-# features$mito <- -log10(summary.df$"% of Mapped Reads Mapping to Mitochondria")
-#
-# ctrl <- trainControl(method = "repeatedcv",
-#                      number = 10,
-#                      repeats = 10,
-#                      verboseIter = FALSE,
-#                      savePredictions=TRUE,
-#                      classProbs=TRUE,
-# #                      preProcOptions=list(thres = 0.90),
-#                      summaryFunction = twoClassSummary)
-# set.seed(1234)
-# model_gbm <- caret::train(type ~ .,
-#                                data = features,
-#                                method = 'gbm',
-#                                tuneGrid=data.frame(n.trees=150,
-#                                                    interaction.depth=3,
-#                                                    shrinkage=0.1,
-#                                                    n.minobsinnode=10),
-#                                preProcess = c("corr", "nzv"),
-#                          trControl = ctrl)
-#
 # ####### Only short/total coverage
-# set.seed(1234)
-# model_sl <- caret::train(type ~ .,
-#                                data = features.sl,
-#                                method = 'gbm',
-#                                tuneGrid=data.frame(n.trees=150, interaction.depth=3,
-#                                                    shrinkage=0.1,
-#                                                    n.minobsinnode=10),
-#                                preProcess = c("corr", "nzv"),
-#                          trControl = ctrl)
-#
-# ###### Only z-scores
-# features.z <- summary.df %>% ungroup() %>% select(contains("Z Score"))
-# features.z$type <- ifelse(summary.df$type == "Healthy", "Healthy", "Cancer")
-#
-# set.seed(1234)
-# model_z <- caret::train(type ~ .,
-#                                data = features.z,
-#                                method = 'gbm',
-#                                tuneGrid=data.frame(n.trees=150,
-#                                                    interaction.depth=3,
-#                                                    shrinkage=0.1,
-#                                                    n.minobsinnode=10),
-#                                preProcess = c("corr", "nzv"),
-#                          trControl = ctrl)
-#
-# #### Save
-# models.list <- list("all"=model_gbm, "SL"=model_sl, "z"=model_z)
-# saveRDS(models.list, "../inst/extdata/models_list.rds")
-#
-# pred.tbl <- model_gbm$pred %>% filter(n.trees==150, interaction.depth==3) %>%
-# group_by(rowIndex) %>% dplyr::summarize(obs=obs[1], Cancer=mean(Cancer))
-# pred.tbl$sample <- rownames(features)
-# pred.tbl <- inner_join(pred.tbl, summary.df)
-#
+set.seed(1234)
+model_sl <- caret::train(
+  type ~ .,
+  data = features.sl,
+  method = 'gbm',
+  tuneGrid = data.frame(
+    n.trees = 150,
+    interaction.depth = 3,
+    shrinkage = 0.1,
+    n.minobsinnode = 10
+  ),
+  preProcess = c("corr", "nzv"),
+  trControl = ctrl
+)
+
+# Save models
+models.list <- list("SL"=model_sl) #"all"=model_gbm, "z"=model_z)
+saveRDS(models.list, opt$out_model_file)
+
+pred.tbl <- model_sl$pred %>%
+  dplyr::as_tibble() %>%
+  filter(n.trees==150, interaction.depth==3)
+
+# Save predictions before averaging them
+write.csv(pred.tbl, file = opt$out_preds_file)
+
+# Average predictions and add sample
+pred.tbl <- pred.tbl %>%
+  group_by(rowIndex) %>%
+  dplyr::summarize(
+    obs=obs[1],
+    Cancer=mean(Cancer)
+  )
+pred.tbl$sample <-  meta_data[[1]]
+
+# Save averaged predictions
+write.csv(pred.tbl, file = paste0(str_sub(opt$out_preds_file, end= -5)), ".avg.csv")
+
 # ## 95% specificity
 # cutoff <- (pred.tbl %>% filter(type=="Healthy") %>%
 #            arrange(desc(Cancer)))$Cancer[11]
